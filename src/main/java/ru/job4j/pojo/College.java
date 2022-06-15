9aeaa7576c1ce67dc209486e1140a3cc02340235
Alexander <aseldom@yandex.ru>
2022-06-15 14:24:45 +0700

    Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Petr Arsentev");
+        student.setGroup("Biolog");
+        student.setDate("2022.01.01");
+        System.out.println("Студент - " + student.getName() + ", группа - " + student.getGroup() + ", дата: "
+        + student.getDate());
+    }
+}
