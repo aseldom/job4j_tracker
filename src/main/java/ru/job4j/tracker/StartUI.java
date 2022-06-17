077050dbfa90802cca31393880bed258140a5d7b
Alexander <aseldom@yandex.ru>
2022-06-17 11:57:35 +0700

    toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,9 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item date = new Item();
+        Item task1 = new Item("Wash car");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(date.getCreated().format(formatter));
+        System.out.println(task1);
     }
 }
