09be40e91cc2c1202495c85fa2b0aacada056975
Alexander <aseldom@yandex.ru>
2022-06-20 14:02:07 +0700

    Интерфейс Input [#181092 #314324]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,16 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-
+        System.out.println("Drive");
     }
 
     @Override
-    public void passengers() {
-
+    public void passengers(int peoples) {
+        System.out.println("Passengers = " + peoples);
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        int cost = 32;
+        return fuel * cost;
     }
 }
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -4,7 +4,7 @@ public interface Transport {
 
     void drive();
 
-    void passengers();
+    void passengers(int peoples);
 
     int refuel(int fuel);
 }
