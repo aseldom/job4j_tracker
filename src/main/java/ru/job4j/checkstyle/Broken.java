8e26b50e588b91af5ff7c4a03c45911f2bca35c1
Alexander <aseldom@yandex.ru>
2023-02-08 18:18:25 +0600

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,27 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEW_VALUE = "";
+    private int sizeOfEmpty = 10;
+    String name;
+    public String surname;
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int b, int c, int d, int e, int f, int g, int h) {
+
+    }
+
+    Broken() {
+
+    }
+}
