89225b3367e887eee1c5ed9251388a3f5ba4a0e2
Alexander <aseldom@yandex.ru>
2023-02-22 20:10:51 +0600

    4. Принципы раннего возврата и охранных выражений [#504875 #393922]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,6 +1,7 @@
 package ru.job4j.early;
 
 import static java.lang.Character.*;
+import static java.lang.Character.isDigit;
 
 public class PasswordValidator {
     private final static String[] PATTERN = {"qwerty", "12345", "password", "admin", "user"};
@@ -11,31 +12,19 @@ public class PasswordValidator {
             throw new IllegalArgumentException("Password can't be null");
         }
 
-        boolean[] check = checkSymbols(password.toCharArray());
-
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
 
-        if (!check[0]) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-        }
-        if (!check[1]) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-        if (!check[2]) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-        if (!check[3]) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
+        checkSymbols(password.toCharArray());
+
         if (isPattern(PATTERN, password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
         }
         return password;
     }
 
-    public static boolean[] checkSymbols(char[] chars) {
+    public static void checkSymbols(char[] chars) {
         boolean isUpper = false;
         boolean isLower = false;
         boolean isDigit = false;
@@ -54,8 +43,22 @@ public class PasswordValidator {
             if (!isDigit(c) && !isLetter(c)) {
                 isSpecial = true;
             }
+            if (isUpper && isLower && isDigit && isSpecial) {
+                break;
+            }
         }
-        return new boolean[] {isUpper, isLower, isDigit, isSpecial};
+        if (!isUpper) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (!isLower) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        if (!isDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        if (!isSpecial) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
     }
 
     public static boolean isPattern(String[] patterns, String password) {
