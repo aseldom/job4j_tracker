84b91248774fe5218bce15c162ec9e1e038cfc6e
Alexander <aseldom@yandex.ru>
2023-02-22 11:22:56 +0600

    4. Принципы раннего возврата и охранных выражений [#504875 #393922]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,68 +3,63 @@ package ru.job4j.early;
 import static java.lang.Character.*;
 
 public class PasswordValidator {
+    private final static String[] PATTERN = {"qwerty", "12345", "password", "admin", "user"};
+    private static boolean isUpper;
+    private static boolean isLower;
+    private static boolean isDigit;
+    private static boolean isSpecial;
 
     public static String validate(String password) {
+        isUpper = false;
+        isLower = false;
+        isDigit = false;
+        isSpecial = false;
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
+
         char[] chars = password.toCharArray();
+
+        checkSymbols(chars);
 
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (!isUpperCaseLatter(chars)) {
+
+        if (!isUpper) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
         }
-        if (!islowerCaseLatter(chars)) {
+        if (!isLower) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
-        if (!isOneFigure(chars)) {
+        if (!isDigit) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
-        if (!isOneSpecial(chars)) {
+        if (!isSpecial) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
-        if (isPattern(new String[] {"qwerty", "12345", "password", "admin", "user"}, password)) {
+        if (isPattern(PATTERN, password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
         }
         return password;
     }
 
-    public static boolean isUpperCaseLatter(char[] chars) {
+    public static void checkSymbols(char[] chars) {
         for (char c : chars) {
             if (isUpperCase(c)) {
-                return true;
+                isUpper = true;
             }
-        }
-        return false;
-    }
-
-    public static boolean islowerCaseLatter(char[] chars) {
-        for (char c : chars) {
             if (isLowerCase(c)) {
-                return true;
+                isLower = true;
             }
-        }
-        return false;
-    }
-
-    public static boolean isOneFigure(char[] chars) {
-        for (char c : chars) {
             if (isDigit(c)) {
-                return true;
+                isDigit = true;
             }
-        }
-        return false;
-    }
-
-    public static boolean isOneSpecial(char[] chars) {
-        for (char c : chars) {
             if (!isDigit(c) && !isLetter(c)) {
-                return true;
+                isSpecial = true;
             }
         }
-        return false;
     }
 
     public static boolean isPattern(String[] patterns, String password) {
--- src/main/java/ru/job4j/record/PersonRecord.java
+++ src/main/java/ru/job4j/record/PersonRecord.java
@@ -3,9 +3,8 @@ package ru.job4j.record;
 public record PersonRecord(String name, int age) {
     @Override
     public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
+        return "Person{"
+                + "name='" + name + '\''
+                + ", age=" + age + '}';
     }
 }
