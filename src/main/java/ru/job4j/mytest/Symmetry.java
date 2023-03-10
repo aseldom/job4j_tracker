b57e8aa48ea281d65404e3c230c9a5db6a1844a7
Alexander <aseldom@yandex.ru>
2023-03-10 10:47:22 +0600

    2. test

---
+++ src/main/java/ru/job4j/mytest/Symmetry.java
@@ -0,0 +1,13 @@
+package ru.job4j.mytest;
+
+public class Symmetry {
+    public static boolean check(int i) {
+        String number = String.valueOf(i);
+        for (int k = 0; k < number.length() / 2; k++) {
+            if (number.charAt(k) == number.charAt(number.length() - k - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
