6bbb5d33d5730d6ab884d32684315fa3e7682a98
Alexander <aseldom@yandex.ru>
2023-03-10 13:30:23 +0600

    1. Паспорт и жители [#212656 #398982]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -11,4 +11,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport())).isEqualTo(citizen);
     }
+
+    @Test
+    public void whenTestAddDoubleNumberPasportThenFalse() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Vasily Terkin");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertThat(office.add(citizen2)).isFalse();
+    }
 }
