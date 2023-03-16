22ee424e78afa29cfcbed019686424a8ed369197
aseldom <a.seldom@gmail.com>
2023-03-16 09:08:57 +0600

    0. Сортировка [#10096 #400755]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
     private int id;
@@ -54,19 +55,12 @@ public class Item {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-
         Item item = (Item) o;
-
-        if (id != item.id) {
-            return false;
-        }
-        return name.equals(item.name);
+        return id == item.id && Objects.equals(name, item.name);
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + name.hashCode();
-        return result;
+        return Objects.hash(id, name);
     }
 }
