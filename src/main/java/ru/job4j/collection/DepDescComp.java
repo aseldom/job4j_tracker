5f62b03b6852611ff56d13e411b81e0059a94432
aseldom <a.seldom@gmail.com>
2023-03-21 15:48:29 +0600

    3. Отсортировать департаменты [#34352 #402620]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,9 +7,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String str1 = o1.split("/")[0];
         String str2 = o2.split("/")[0];
-        if (str1.compareTo(str2) == 0) {
-            return o1.compareTo(o2);
-        }
-        return o2.compareTo(o1);
+        int result = str2.compareTo(str1);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
